3a70d65841d7e67e25d4bc3dcc78fad134076316
MuhamadRizky14 <75973643+MuhamadRizky14@users.noreply.github.com>
2021-05-18 00:20:40 +0700

    Menyelesaikan tugas praktikum

---
+++ Praktikum/pertemuan5/latihan5e/index.php
@@ -0,0 +1,52 @@
+<!-- 
+    NAMA : Muhamad Rizky
+    NPM : 203040043
+    Kelas : B
+    Github : https://github.com/MuhamadRizky14
+    MataKuliah : Pemrograman Web
+    Shift : Jumat, 10.00 WIB
+ -->
+ 
+<?php
+    require 'php/functions.php';
+    $mahasiswa = query("SELECT * FROM mahasiswa")
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>latihan5e</title>
+    <link rel="stylesheet" href="css/style2.css">
+</head>
+<body>
+    <button><a href="php/admin.php">Edit</a></button>
+    <table class="table" border="1" cellpadding="15" cellspacing="0">
+        <tr>
+          <th>No</th>
+          <th>Foto</th>
+          <th>NRP</th>
+          <th>Nama</th>
+          <th>Email</th>
+          <th>Jurusan</th>
+          <th>#</th>
+        </tr>
+		<?php $i = 1; ?>
+		<?php foreach ($mahasiswa as $mhs) : ?>
+         <tr>
+             <td><?=$i; ?></td>
+             <td><img src="assets/img/<?=$mhs["img"]; ?>"></td>
+             <td><?=$mhs["nrp"]; ?></td>
+             <td><?=$mhs["nama"]; ?></td>
+             <td><?=$mhs["email"]; ?></td>
+             <td><?=$mhs["jurusan"]; ?></td>
+             <td>
+                <a href="php/detail.php?id=<?=$mhs['id'] ?>" style="text-decoration: none;">Lihat</a>
+            </td>
+         </tr>
+         <?php $i++;?>
+        <?php endforeach; ?>
+    </table>
+</body>
+</html>
