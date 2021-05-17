a9c65380e6c233d4adb9e7f5e24614b8c7274bd1
MuhamadRizky14 <75973643+MuhamadRizky14@users.noreply.github.com>
2021-05-18 00:11:36 +0700

    Menyelesaikan tugas praktikum

---
+++ Praktikum/pertemuan1/tugas1.php
@@ -0,0 +1,31 @@
+s<!-- 
+Nama : Muhamad Rizky
+NRP : 203040043
+Shift Praktikum : Jum'at pukul 10:00
+ -->
+
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas 1</title>
+
+</head>
+<body>
+
+<table width="230px" cellspacing="5px" cellpadding="0px" border="1px">
+
+<?php for( $row = 1; $row <= 6; $row++ ) : ?>
+    <tr>
+        <?php for( $col = 1; $col <= 6; $col++ ) : ?>
+            <?php $all = $row + $col; ?>
+            <?php if( $all % 2 == 1 ) : ?>
+                <td height=30px width=30px bgcolor=salmon></td>
+            <?php else : ?>
+                <td height=30px width=30px bgcolor=lightblue></td>
+            <?php endif; ?>
+        <?php endfor; ?> 
+    </tr>
+<?php endfor; ?>
