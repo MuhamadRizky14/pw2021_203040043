f25fa6d624095fed748096311781224327ad77e6
MuhamadRizky14 <75973643+MuhamadRizky14@users.noreply.github.com>
2021-05-30 10:00:49 +0700

    Menyelesaikan tugas praktikum

---
+++ praktikum/pertemuan1/latihan1a.php
@@ -0,0 +1,15 @@
+<!-- 
+Nama : Muhamad Rizky
+NRP : 203040043
+Shift Praktikum : Jum'at pukul 10:00
+ -->
+
+ <?php
+
+for( $i = 1; $i <= 3; $i++ ) {
+    for( $j = 1; $j <= 3; $j++ ) {
+        echo "Ini perulangan ke ($i,$j) <br>";
+    }
+}
+
+?>
---
+++ praktikum/pertemuan2/latihan2b.php
@@ -0,0 +1,47 @@
+<!-- 
+Nama : Muhamad Rizky
+NRP : 203040043
+Shift Praktikum : Jum'at pukul 10:00
+Pertemuan : 2
+ -->
+
+ <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan 2 B</title>
+
+    <style>
+        .kotak {
+            border: 2px solid black;
+            padding: 10px;
+        }
+    </style>
+</head>
+
+<body>
+
+    <?php
+
+    $jawabanIsset = "Isset adalah = Berfungsi untuk memeriksa apakah suatu variabel sudah diatur atau belum. Fungsi isset juga mengembalikan false jika variabel pengujian berisi nilai NULL. <br><br>";
+    $jawabanEmpty = "Empty adalah = Berfungsi sebagai penanda suatu kondisi. Contohnya yang sering kita temukan adalah dalam pengisian suatu data, apakah data tersebut ada isinya atau tidak.";
+
+    function soal($style)
+    {
+        global $jawabanIsset, $jawabanEmpty;
+        echo $jawabanIsset, $jawabanEmpty;
+        return $style;
+    }
+
+    ?>
+
+    <div class="kotak">
+        <?= soal("  "); ?>
+    </div>
+
+</body>
+
+</html>
