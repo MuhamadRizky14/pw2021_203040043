17cc5e5f821749a309e97febd9c5e11a2646c1e6
MuhamadRizky14 <75973643+MuhamadRizky14@users.noreply.github.com>
2021-05-30 10:10:13 +0700

    Menyelesaikan tugas praktikum

---
+++ praktikum/pertemuan5/latihan5a/php/functions.php
@@ -0,0 +1,28 @@
+<!-- 
+    NAMA : Muhamad Rizky
+    NPM : 203040043
+    Kelas : B
+    Github : https://github.com/MuhamadRizky14
+    MataKuliah : Pemrograman Web
+    Shift : Jumat, 10.00 WIB
+ -->
+
+ <?php
+function koneksi()
+{
+    $conn = mysqli_connect("localhost", "root", "");
+    mysqli_select_db($conn, "pw_tubes_203040043");
+
+    return $conn;
+}
+
+function query ($sql)
+{
+    $conn = koneksi();
+    $result = mysqli_query($conn, "$sql");
+    $rows = [];
+    while ($row = mysqli_fetch_assoc($result)) {
+        $rows[] = $row;
+    }
+    return $rows;
+}
---
+++ praktikum/pertemuan5/latihan5c/php/admin.php
@@ -0,0 +1,59 @@
+<!-- 
+    NAMA : Muhamad Rizky
+    NPM : 203040043
+    Kelas : B
+    Github : https://github.com/MuhamadRizky14
+    MataKuliah : Pemrograman Web
+    Shift : Jumat, 10.00 WIB
+ -->
+
+
+ <?php
+    require 'functions.php';
+    $mahasiswa = query("SELECT * FROM mahasiswa")
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>latihan5c</title>
+    <link rel="stylesheet" href="../css/style2.css">
+</head>
+<body>
+    <div class="add">
+        <a href="tambah.php"><button>Tambah Data</button></a>
+    </div>
+    <br>
+    <table class="table" border="1" cellpadding="13" cellspacing="0">
+        <tr>
+            <th>No</th>
+            <th>Opsi</th>
+            <th>Foto</th>
+            <th>NRP</th>
+            <th>Nama</th>
+            <th>Email</th>
+            <th>Jurusan</th>
+        </tr>
+        <?php $i = 1;?>
+        <?php foreach ($mahasiswa as $mhs) : ?>
+            <tr>
+                <td><?= $i; ?></td>
+                <td>
+                    <a href=""><button>Ubah</button></a>
+                    <a href="hapus.php?id=<?=$mhs['id'] ?>" onclick="return confirm('Hapus Data??')"><button>Hapus</button></a>
+                </td>
+                <td><img src="../assets/img/<?= $mhs['img']; ?>" alt=""></td>
+                <td><?= $mhs['nrp']; ?></td>
+                <td><?= $mhs['nama']; ?></td>
+                <td><?= $mhs['email']; ?></td>
+                <td><?= $mhs['jurusan']; ?></td>
+            </tr>
+            <?php $i++; ?>
+        <?php endforeach; ?>
+    </table>
+
+</body>
+</html>
